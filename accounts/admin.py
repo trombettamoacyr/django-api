6ad091c3e3574deae92daf0868fb47b75512d498
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2022-02-10 02:32:44 -0300

    creation models - Category and Transaction

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from accounts.models import Category
+from accounts.models import Transaction
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Transaction)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    date = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=200)
+    value = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    notes = models.TextField(max_length=500, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'transactions'
+
+    def __str__(self):
+        return self.description
